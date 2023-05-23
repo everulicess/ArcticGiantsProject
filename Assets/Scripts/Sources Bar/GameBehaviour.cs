a5fcd5f72d7d0b7064d8b18c97e3d7c075b94998
everulicess <ulices.vaca12@gmail.com>
2023-05-23 14:58:11 +0200

    counter

--- Assets/Scripts/Sources Bar/GameBehaviour.cs
+++ Assets/Scripts/Sources Bar/GameBehaviour.cs
@@ -1,10 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameBehaviour : MonoBehaviour
 {
+    //Lights
     public int numberOfLightsOn = 5;
+
+    //Countdown
+    public float startingTime = 20f;
+    float currentTime = 0f;
+    [SerializeField]
+    TMP_Text timerText;
+
+    private void Start()
+    {
+        //currentTime = startingTime;
+    }
+    private void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
 
     public int lights
     {
