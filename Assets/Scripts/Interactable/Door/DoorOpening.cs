c7ef64eec971aff7f5513bd84c7f87bf6b676b7a
everulicess <ulices.vaca12@gmail.com>
2023-06-05 15:26:56 +0200

    Revert "SPawningPlayer2Coordinates changed"
    This reverts commit a62d3d1a184a847065b91a417fa9e3f50d893bf5.

--- Assets/Scripts/Interactable/Door/DoorOpening.cs
+++ Assets/Scripts/Interactable/Door/DoorOpening.cs
@@ -6,7 +6,6 @@ public class DoorOpening : MonoBehaviour
 {
     [SerializeField]
     Animator doorAnim;
-    bool isPlayerNear;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +15,19 @@ public class DoorOpening : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //doorAnim.SetBool("openDoor",true);
+        doorAnim.SetBool("openDoor",true);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isPlayerNear = true;
             doorAnim.SetBool("openDoor", true);
         }
     }private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isPlayerNear = false;
             doorAnim.SetBool("openDoor", false);
         }
     }
