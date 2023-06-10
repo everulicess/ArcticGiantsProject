0fc6cfb8f8cd0de44a30f9752b9193a6089e7d0d
everulicess <ulices.vaca12@gmail.com>
2023-06-10 16:02:59 +0200

    Update DoorInteraction.cs

--- Assets/Scripts/Interactable/Door/DoorInteraction.cs
+++ Assets/Scripts/Interactable/Door/DoorInteraction.cs
@@ -95,6 +95,7 @@ public class DoorInteraction : DoorOpening
             }
             else
             {
+                objectHit = null;
                 buttonHit = false;
             }
         }
